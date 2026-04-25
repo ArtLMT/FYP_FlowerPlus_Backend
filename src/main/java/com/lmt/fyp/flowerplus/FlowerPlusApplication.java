0ba443126f735e3785418d0aa25608f3dd4331b5
Lê Minh Thành <leminhthanh280425@gmail.com>
2026-04-25 14:01:13 +0700

    first commit

---
+++ src/main/java/com/lmt/fyp/flowerplus/FlowerPlusApplication.java
@@ -0,0 +1,13 @@
+package com.lmt.fyp.flowerplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowerPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowerPlusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lmt/fyp/flowerplus/FlowerPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmt.fyp.flowerplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowerPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
