af6eb8d90d8299084d823b445491427f15ee4504
Lê Minh Thành <leminhthanh280425@gmail.com>
2026-05-04 16:34:35 +0700

    feat: add docker-compose for postgres and configure db connection

--- src/main/java/com/lmt/fyp/flowerplus/FlowerPlusApplication.java
+++ src/main/java/com/lmt/fyp/flowerplus/FlowerPlusApplication.java
@@ -1,12 +1,16 @@
 package com.lmt.fyp.flowerplus;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class FlowerPlusApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
         SpringApplication.run(FlowerPlusApplication.class, args);
     }
 
